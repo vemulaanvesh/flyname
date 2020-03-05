a3d7ad0c8c622b75bef0bb64859a242005d7d112
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2020-03-05 13:00:30 +0530

    Update and rename V3__ sample3.sql to V6__ sample3.sql

--- my/sqldir/V6__ sample3.sql
+++ my/sqldir/V6__ sample3.sql
@@ -6,4 +6,4 @@ create table PERSON  (
 insert into PERSON (ID, NAME) values (1, 'Axel');
 insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
 insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
-insert into PERSON (ID, NAME) values (3, 'Ms. Bard');
+
