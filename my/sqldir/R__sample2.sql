7481ba788fc3db0da3e4fa5fb2a4d2211435f925
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2020-03-05 12:22:12 +0530

    Update and rename R__sample2.sql to V1__sample2.sql

--- my/sqldir/R__sample2.sql
+++
@@ -1,8 +0,0 @@
-create table anvesh(
- id INT NOT NULL,
- name varchar(45)
-);
-create table prithvi(
- id INT NOT NULL,
- name varchar(45)
-);
---
+++ my/sqldir/V1__sample2.sql
@@ -0,0 +1,5 @@
+create table anvesh(
+ id INT NOT NULL,
+ name varchar(45)
+);
+
