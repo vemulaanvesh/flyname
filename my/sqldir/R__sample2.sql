6f837b3dc600f4d3470d3d20ab65817b97ba32d2
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2020-03-06 17:49:57 +0530

    Update R__sample2.sql

--- my/sqldir/R__sample2.sql
+++ my/sqldir/R__sample2.sql
@@ -1,14 +1,12 @@
 
 
-CREATE TABLE [IF NOT EXISTS] dbo.anvesh
-(
- id INT NOT NULL,
- name varchar(45),
-);
+IF NOT EXISTS (SELECT * FROM flyway1.dbo.anvesh)
+    CREATE TABLE anvesh (
+     id INT NOT NULL,
+     name varchar(45));
 
-CREATE TABLE [IF NOT EXISTS] dbo.lenin
-(
- id INT NOT NULL,
- name varchar(45),
-);
+IF NOT EXISTS (SELECT * FROM flyway1.dbo.lenin)
+    CREATE TABLE anvesh (
+     id INT NOT NULL,
+     name varchar(45));
 
