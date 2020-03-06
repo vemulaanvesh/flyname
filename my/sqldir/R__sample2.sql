0392671ed6fb6464185acabf4be60cdb0622dce2
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2020-03-06 17:06:04 +0530

    Update R__sample2.sql

--- my/sqldir/R__sample2.sql
+++ my/sqldir/R__sample2.sql
@@ -1,7 +1,10 @@
+DROP TABLE IF EXISTS anvesh;
 create table IF NOT EXISTS anvesh(
  id INT NOT NULL,
  name varchar(45)
 );
+
+DROP TABLE IF EXISTS lenin;
 create table IF NOT EXISTS lenin(
  id INT NOT NULL,
  name varchar(45)
