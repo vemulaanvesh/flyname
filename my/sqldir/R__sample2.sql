e72d2cfc87e871512acd2a2f874c2003e32c02fc
vemulaanvesh <39478419+vemulaanvesh@users.noreply.github.com>
2020-03-06 17:36:16 +0530

    Update R__sample2.sql

--- my/sqldir/R__sample2.sql
+++ my/sqldir/R__sample2.sql
@@ -1,12 +1,12 @@
 
 
-CREATE TABLE [IF NOT EXISTS] anvesh
+CREATE TABLE [IF NOT EXISTS] dbo.anvesh
 (
  id INT NOT NULL,
  name varchar(45),
 );
 
-CREATE TABLE [IF NOT EXISTS] lenin
+CREATE TABLE [IF NOT EXISTS] dbo.lenin
 (
  id INT NOT NULL,
  name varchar(45),
